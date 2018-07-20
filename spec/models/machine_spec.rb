c2870e4fea4c6f4d580a1646704f46f8731bc853
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-20 10:54:49 -0600

    Add instance methods tests for the machine model.

---
+++ spec/models/machine_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Machine, type: :model do
+  describe 'Instance Methods' do
+    it '#average_snack_price' do
+      owner = Owner.create(name: "Sam's Snacks")
+      machine = owner.machines.create(location: "Don's Mixed Drinks", owner_id: owner.id )
+      machine.snacks.create(name: 'Mentos', price: 1.00)
+      machine.snacks.create(name: 'Twizzlers', price: 2.00)
+      machine.snacks.create(name: 'Chips', price: 3.00)
+
+      expect(machine.average_snack_price).to eq(2.00)
+    end
+
+    it '#snack_count' do
+      owner = Owner.create(name: "Sam's Snacks")
+      machine = owner.machines.create(location: "Don's Mixed Drinks", owner_id: owner.id )
+      machine.snacks.create(name: 'Mentos', price: 1.95)
+      machine.snacks.create(name: 'Twizzlers', price: 1.35)
+      machine.snacks.create(name: 'Chips', price: 1.55)
+
+      expect(machine.snack_count).to eq(3)
+    end
+  end
+end
