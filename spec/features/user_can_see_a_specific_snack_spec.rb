4cd4d28d82e1478c2cbec3f23ba87d765fe759af
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-20 10:39:55 -0600

    Create snack show page; user can currently see the snack name and price and the vending machine locations that carry the snack.

---
+++ spec/features/user_can_see_a_specific_snack_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+
+describe "User visits 'snacks/:id'" do
+  it 'they can see the name and price of the snack' do
+    snack = Snack.create(name: 'Snickers', price: 1.50)
+
+    visit snack_path(snack)
+
+    expect(page).to have_content(snack.name)
+    expect(page).to have_content("Price: $#{snack.price}")
+  end
+
+  it 'they can see a list of locations with vending machines that carry that snack' do
+    snack = Snack.create(name: 'Snickers', price: 1.50)
+    owner = Owner.create(name: "Sam's Snacks")
+    machine_1 = snack.machines.create(location: "Don's Mixed Drinks", owner_id: owner.id )
+    machine_2 = snack.machines.create(location: "Turing Basement", owner_id: owner.id)
+
+    visit snack_path(snack)
+
+    within '#locations' do
+      expect(page).to have_content(machine_1.location)
+      expect(page).to have_content(machine_2.location)
+    end
+  end
+
+  it 'they see the average price of snack in each vending machine' do
+
+  end
+
+  it 'they see a count of the different kinds of snacks in that vending machine' do
+
+  end
+end
