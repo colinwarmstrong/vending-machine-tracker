51edfd93e17e600c0c7983a4fc8e17e18c3d46ec
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-20 10:43:35 -0600

    Add average snack price for each vending machine on the snack show page.

--- spec/features/user_can_see_a_specific_snack_spec.rb
+++ spec/features/user_can_see_a_specific_snack_spec.rb
@@ -26,10 +26,30 @@ describe "User visits 'snacks/:id'" do
   end
 
   it 'they see the average price of snack in each vending machine' do
+    snack = Snack.create(name: 'Snickers', price: 1.50)
+    owner = Owner.create(name: "Sam's Snacks")
+    machine_1 = snack.machines.create(location: "Don's Mixed Drinks", owner_id: owner.id )
+    machine_2 = snack.machines.create(location: "Turing Basement", owner_id: owner.id)
 
+    visit snack_path(snack)
+
+    within '#locations' do
+      expect(page).to have_content("Average Price for Vending Machine: $#{machine_1.average_snack_price}")
+      expect(page).to have_content("Average Price for Vending Machine: $#{machine_2.average_snack_price}")
+    end
   end
 
   it 'they see a count of the different kinds of snacks in that vending machine' do
+    snack = Snack.create(name: 'Snickers', price: 1.50)
+    owner = Owner.create(name: "Sam's Snacks")
+    machine_1 = snack.machines.create(location: "Don's Mixed Drinks", owner_id: owner.id )
+    machine_2 = snack.machines.create(location: "Turing Basement", owner_id: owner.id)
 
+    visit snack_path(snack)
+
+    within '#locations' do
+      expect(page).to have_content("Average Price for Vending Machine: $#{machine_1.average_snack_price}")
+      expect(page).to have_content("Average Price for Vending Machine: $#{machine_2.average_snack_price}")
+    end
   end
 end
